f235595b22b2a491448e602618525a8abdc1b297
Emanuele Manzione <m4nu.91@gmail.com>
2020-11-30 10:05:51 +0000

    Few optimizations in LzoXtea class

--- Core/Core/Types/LzoXtea.cs
+++ Core/Core/Types/LzoXtea.cs
@@ -5,30 +5,32 @@ namespace QuantumCore.Core.Types
 {
     public class LzoXtea
     {
+        private static readonly uint[] Keys = { 0x2A4A1, 0x45415AA, 0x185A8BE7, 0x1AAD6AB };
+        private const string ItemProtoDecryptedName = "item_proto_decrypted";
+
         private uint _size;
         private uint _xteaSize;
+
+        private readonly Lzo _lzoInstance;
         
         public LzoXtea(uint size, uint xteaSize)
         {
             _size = size;
             _xteaSize = xteaSize;
+            _lzoInstance = new Lzo(size);
         }
 
         public byte[] Decode(byte[] input)
         {
-            uint[] key = {
-                0x2A4A1, 0x45415AA, 0x185A8BE7, 0x1AAD6AB
-            };
-            var decrypted = Xtea.Decrypt(input, _xteaSize, key, 32); //188836
-            File.WriteAllBytes("item_proto_decrypted", decrypted);
+            var decrypted = Xtea.Decrypt(input, _xteaSize, Keys, 32); //188836
+            File.WriteAllBytes(ItemProtoDecryptedName, decrypted);
             if (decrypted[0] != 'M' || decrypted[1] != 'C' || decrypted[2] != 'O' || decrypted[3] != 'Z')
             {
                 throw new InvalidDataException("Failed to decrypt data stream");
             }
             //decrypted = Xtea.Decrypt(decrypted, 188856, key, 32); //188836
-            var lzo = new Lzo(_size);
             
-            return lzo.Decode(decrypted);
+            return _lzoInstance.Decode(decrypted);
         }
     }
 }
