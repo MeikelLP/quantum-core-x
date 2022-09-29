dd041d7ae86ec67af44b7a5cf6df9c6df74e7145
Meikel Philipp <MeikelLP@users.noreply.github.com>
2022-09-30 01:12:26 +0200

    CHG discard help command implementation (for now)

--- Core/Game/Commands/HelpCommand.cs
+++ Core/Game/Commands/HelpCommand.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using QuantumCore.API.Game;
 using QuantumCore.API.Game.World;
@@ -13,83 +10,15 @@ namespace QuantumCore.Game.Commands
     public static class HelpCommand
     {
         [CommandMethod]
-        public static async Task Help(IPlayerEntity player, ICommandManager commandManager, int page = 1)
+        public static Task Help(IPlayerEntity player, ICommandManager commandManager, int page = 1)
         {
-            var usableCmd = new Dictionary<string, CommandCache>();
-
-            foreach (var cmd in commandManager.Commands)
-            {
-                if (commandManager.CanUseCommand((World.Entities.PlayerEntity) player, cmd.Key))
-                    usableCmd.Add(cmd.Key, cmd.Value);
-            }
-
-            if (usableCmd.Count < 1)
-            {
-                await player.SendChatInfo("--- Help - Page 0/0 ---");
-            }
-            else
-            {
-
-                var allPages = (int) Math.Ceiling(usableCmd.Count / 5.0);
-
-                if (page > allPages)
-                    page = allPages;
-
-                await player.SendChatInfo($"--- Help - Page {page}/{allPages} ---");
-
-                var commandToShow = page * 5;
-
-                if (commandToShow > usableCmd.Count)
-                    commandToShow = usableCmd.Count;
-
-                for (var i = (page - 1) * 5; i < commandToShow; i++)
-                {
-                    var command = usableCmd.ElementAt(i);
-                    await player.SendChatInfo($"{command.Key}: {command.Value.Description}");
-                }
-            }
+            throw new NotImplementedException();
         }
 
         [CommandMethod("Shows an help with a specific command")]
-        public static async Task HelpWithCommand(IPlayerEntity player, ICommandManager commandManager, string command)
+        public static Task HelpWithCommand(IPlayerEntity player, ICommandManager commandManager, string command)
         {
-            if (!commandManager.Commands.ContainsKey(command) || !commandManager.CanUseCommand((World.Entities.PlayerEntity) player, command))
-            {
-                await player.SendChatInfo("Specified command does not exists");
-            }
-            else
-            {
-                var key = commandManager.Commands[command];
-
-                await player.SendChatInfo($"--- Help for command {command} ---");
-
-                string commandString;
-
-                foreach (var desc in key.Functions)
-                {
-                    commandString = "";
-
-                    foreach (var param in desc.Method.GetParameters())
-                    {
-                        if (param.Position == 0)
-                            continue;
-
-                        if (param.HasDefaultValue)
-                            commandString += " [";
-                        else
-                            commandString += " <";
-
-                        commandString += $"{param.ParameterType.Name}:{param.Name}";
-
-                        if (param.HasDefaultValue)
-                            commandString += "]";
-                        else
-                            commandString += ">";
-                    }
-
-                    await player.SendChatInfo($"{command}{commandString}: {desc.Description}");
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
