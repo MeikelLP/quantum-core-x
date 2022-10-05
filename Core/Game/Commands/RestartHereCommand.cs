db1666717772b7cb0d869299f67a09325cfce3b3
Meikel Philipp <MeikelLP@users.noreply.github.com>
2022-10-05 20:55:46 +0200

    CHG prepared restart commands but no tests (TODO)

---
+++ Core/Game/Commands/RestartHereCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using QuantumCore.API.Game;
+
+namespace QuantumCore.Game.Commands
+{
+    [Command("restart_here", "Respawns here")]
+    [CommandNoPermission]
+    public class RestartHereCommand : ICommandHandler
+    {
+        public async Task ExecuteAsync(CommandContext context)
+        {
+            await context.Player.Respawn(false);
+        }
+    }
+}
---
+++ Core/Game/Commands/RestartTownCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using QuantumCore.API.Game;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("restart_town", "Respawns in town")]
+[CommandNoPermission]
+public class RestartTownCommand : ICommandHandler
+{
+    public async Task ExecuteAsync(CommandContext context)
+    {
+        await context.Player.Respawn(true);
+    }
+}
