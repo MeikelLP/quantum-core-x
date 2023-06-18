ebcadd2ba9d70d90125772e2cde2606937c1933e
Ufuk Yucel <uufukyucel@gmail.com>
2023-06-19 00:52:38 +0300

    add affect to db, send packets

---
+++ Core/Game/Items/EPointTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumCore.Game.Items
+{
+    public enum EPointTypes
+    {
+        AttackSpeed = 17,
+        MoveSpeed = 19
+    }
+}
