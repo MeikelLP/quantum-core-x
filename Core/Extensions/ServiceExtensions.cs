a836fb989834f77fea63836056c59c50fdaa098f
Meikel Philipp <MeikelLP@users.noreply.github.com>
2023-06-24 01:53:47 +0200

    CHG simplify log messages (#10)

--- Core/Extensions/ServiceExtensions.cs
+++ Core/Extensions/ServiceExtensions.cs
@@ -20,8 +20,7 @@ namespace QuantumCore.Extensions;
 
 public static class ServiceExtensions
 {
-    private const string MessageTemplate = "[{Timestamp:HH:mm:ss.fff}][{Level:u3}][{ProcessName:u5}|{MachineName}:" +
-                                           "{EnvironmentUserName}]{Caller} >> {Message:lj} " +
+    private const string MessageTemplate = "[{Timestamp:HH:mm:ss.fff}][{Level:u3}]{Message:lj} " +
                                            "{NewLine:1}{Exception:1}";
     public static IServiceCollection AddCoreServices(this IServiceCollection services, IPluginCatalog pluginCatalog)
     {
