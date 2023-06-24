c407015cecedb226c8fdc946af56f2acfead46b6
Meikel Philipp <MeikelLP@users.noreply.github.com>
2023-06-24 12:56:57 +0200

    DEL unused method

--- Core.Caching/CacheManager.cs
+++ Core.Caching/CacheManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Data;
-using BeetleX.Redis;
-using Dapper.Contrib.Extensions;
+﻿using BeetleX.Redis;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -8,38 +6,16 @@ namespace QuantumCore.Caching;
 
 public class CacheManager : ICacheManager
 {
-    private readonly IDbConnection _db;
     private readonly ILogger<CacheManager> _logger;
     private readonly RedisDB _redis;
 
-    public CacheManager(IDbConnection db, ILogger<CacheManager> logger, IOptions<CacheOptions> options)
+    public CacheManager(ILogger<CacheManager> logger, IOptions<CacheOptions> options)
     {
-        _db = db;
         _logger = logger;
         _redis = new RedisDB { DataFormater = new JsonFormater() };
         _logger.LogInformation("Initialize Cache Manager");
         _redis.Host.AddWriteHost(options.Value.Host, options.Value.Port);
     }
-
-    public async ValueTask<T> GetOrCreate<T>(object id) where T : class
-    {
-        var keyName = $"{typeof(T).Name}:{id}";
-
-        if (await _redis.Exists(keyName) > 0)
-        {
-            // We have the object in cache! We're good to go
-            _logger.LogDebug("Found {Type} with id {Id} in cache", typeof(T).Name, id);
-            return await _redis.Get<T>(keyName);
-        }
-
-        _logger.LogDebug("Query {Type} with id {Id} from the database", typeof(T).Name, id);
-        // We have to query the object from the database, cache it and return it
-        var obj = await _db.GetAsync<T>(id);
-        await _redis.Set(keyName, obj);
-            
-        return obj;
-    }
-
     public IRedisListWrapper<T> CreateList<T>(string name) => new RedisListWrapper<T>(_redis.CreateList<T>(name));
     public ValueTask<long> Del(string key) => _redis.Del(key);
     public ValueTask<string> Set(string key, object item) => _redis.Set(key, item);
--- Core.Caching/ICacheManager.cs
+++ Core.Caching/ICacheManager.cs
@@ -6,7 +6,6 @@ public interface ICacheManager
     ValueTask<long> Del(string key);
     ValueTask<string> Set(string key, object item);
     ValueTask<T> Get<T>(string key);
-    ValueTask<T> GetOrCreate<T>(object id) where T : class;
     ValueTask<long> Exists(string key);
     ValueTask<long> Expire(string key, int seconds);
     ValueTask<bool> Ping();
