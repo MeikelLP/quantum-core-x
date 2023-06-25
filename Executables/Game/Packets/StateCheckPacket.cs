182576e7e11d3c9affe5efeec556148db0e245df
Meikel Philipp <MeikelLP@users.noreply.github.com>
2023-06-25 17:21:28 +0200

    CHG split classes into multiple files

--- Executables/Game/Packets/StateCheckPacket.cs
+++ Executables/Game/Packets/StateCheckPacket.cs
@@ -7,25 +7,3 @@ namespace QuantumCore.Game.Packets;
 public partial class StateCheckPacket
 {
 }
-
-[Packet(0xD2, EDirection.Outgoing)]
-[PacketGenerator]
-public partial class ServerStatusPacket
-{
-    [Field(0)] public uint Size => (uint)Statuses.Length;
-    
-    [Field(1)]
-    public ServerStatus[] Statuses { get; set; }
-    
-    [Field(2)] public byte IsSuccess { get; set; }
-}
-
-
-public class ServerStatus
-{
-    [Field(0)]
-    public short Port { get; set; }
-    
-    [Field(1)]
-    public byte Status { get; set; }
-}
