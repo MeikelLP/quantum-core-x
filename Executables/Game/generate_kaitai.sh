111f122b7c692af08f8c1fe196aec046eb71c9f7
Meikel Philipp <MeikelLP@users.noreply.github.com>
2023-11-01 16:40:20 +0100

    FIX shell script

--- Executables/Game/generate_kaitai.sh
+++ Executables/Game/generate_kaitai.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+MY_PATH="$(dirname -- "$0")"
 kaitai-struct-compiler $MY_PATH/Types/item_proto.ksy -t csharp --outdir $MY_PATH/Types/ --dotnet-namespace QuantumCore.Core.Types
 kaitai-struct-compiler $MY_PATH/Types/mob_proto.ksy -t csharp --outdir $MY_PATH/Types/ --dotnet-namespace QuantumCore.Core.Types
