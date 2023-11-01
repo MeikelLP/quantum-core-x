73e3e7bb21021d2ad0338100ebbba5e3437b9ad5
Meikel Philipp <MeikelLP@users.noreply.github.com>
2023-11-01 16:03:42 +0100

    FIX ps script

--- Executables/Game/generate_kaitai.ps1
+++ Executables/Game/generate_kaitai.ps1
@@ -1,2 +1,2 @@
-kaitai-struct-compiler $PSScriptRoot/Types/item_proto.ksy -t csharp --outdir Types/ --dotnet-namespace QuantumCore.Core.Types
-kaitai-struct-compiler $PSScriptRoot/Types/mob_proto.ksy -t csharp --outdir Types/ --dotnet-namespace QuantumCore.Core.Types
+kaitai-struct-compiler $PSScriptRoot/Types/item_proto.ksy -t csharp --outdir $PSScriptRoot/Types/ --dotnet-namespace QuantumCore.Core.Types
+kaitai-struct-compiler $PSScriptRoot/Types/mob_proto.ksy -t csharp --outdir $PSScriptRoot/Types/ --dotnet-namespace QuantumCore.Core.Types
