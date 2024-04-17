b86b3bac72510dd101660d476eda5c9c356e897d
Meikel Philipp <MeikelLP@users.noreply.github.com>
2024-04-17 22:34:55 +0200

    CHG move packets from game & auth project to CoreApi

---
+++ src/CorePluginAPI/CharacterMovementType.cs
@@ -0,0 +1,12 @@
+﻿namespace QuantumCore.Game.Packets;
+
+public enum CharacterMovementType : byte
+{
+    Wait = 0,
+    Move = 1,
+    Attack = 2,
+    Combo = 3,
+    MobSkill = 4,
+    Max,
+    Skill = 0x80
+}
