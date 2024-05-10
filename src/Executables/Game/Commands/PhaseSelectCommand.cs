8302bc8c28b6df3f32a217a910de969bdf800add
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-05-10 20:10:14 +0100

    wip

--- src/Executables/Game/Commands/PhaseSelectCommand.cs
+++ src/Executables/Game/Commands/PhaseSelectCommand.cs
@@ -1,7 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
 using QuantumCore.API.Game;
 using QuantumCore.API.Game.Types;
 using QuantumCore.API.Game.World;
+using QuantumCore.Caching;
 using QuantumCore.Extensions;
+using QuantumCore.Game.Persistence;
 
 namespace QuantumCore.Game.Commands
 {
@@ -10,31 +13,37 @@ namespace QuantumCore.Game.Commands
     public class PhaseSelectCommand : ICommandHandler
     {
         private readonly IWorld _world;
+        private readonly IServiceProvider _provider;
+        private readonly ICacheManager _cache;
 
-        public PhaseSelectCommand(IWorld world)
+        public PhaseSelectCommand(IWorld world, IServiceProvider provider, ICacheManager cache)
         {
             _world = world;
+            _provider = provider;
+            _cache = cache;
         }
 
-        public Task ExecuteAsync(CommandContext context)
+        public async Task ExecuteAsync(CommandContext context)
         {
             context.Player.SendChatInfo("Going back to character selection. Please wait.");
 
             // todo implement wait
             
             // Calculate session time
-            var sessionTimeMillis = context.Player.Connection.Server.ServerTime - context.Player.LoggedInTime;
+            var key = $"player:{context.Player.Player.Id}:loggedInTime";
+            var startSessionTime = await _cache.Get<long>(key);
+            
+            var sessionTimeMillis = context.Player.Connection.Server.ServerTime - startSessionTime;
             var minutes = sessionTimeMillis / 60000; // milliseconds to minutes
             
             context.Player.AddPoint(EPoints.PlayTime, (int) minutes);
             
-            // todo implement persist
+            //var manager = ActivatorUtilities.CreateInstance<DbPlayerRepository>(_provider);
+            //await manager.SaveAsync(context.Player.Player);
 
             // Despawn player
             _world.DespawnEntity(context.Player);
             context.Player.Connection.SetPhase(EPhases.Select);
-            
-            return Task.CompletedTask;
         }
     }
 }
