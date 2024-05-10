7c8b90a4f34285d01b2611168d6abca4c6f6158d
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-05-10 22:32:29 +0100

    wip - broken compiation

---
+++ src/Executables/Game/Commands/MallPasswordCommand.cs
@@ -0,0 +1,14 @@
+﻿using QuantumCore.API.Game;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("mall_password", "Opens the in-game item-shop warehouse")]
+[CommandNoPermission]
+public class MallPasswordCommand : ICommandHandler
+{
+    public Task ExecuteAsync(CommandContext context)
+    {
+        context.Player.SendChatCommand("ShowMeMallPassword");
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Executables/Game/Commands/OpenMallCommand.cs
@@ -0,0 +1,14 @@
+﻿using QuantumCore.API.Game;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("click_mall", "Opens the in-game item-shop password prompt")]
+[CommandNoPermission]
+public class OpenMallCommand : ICommandHandler
+{
+    public Task ExecuteAsync(CommandContext context)
+    {
+        context.Player.SendChatCommand("ShowMeMallPassword");
+        return Task.CompletedTask;
+    }
+}
