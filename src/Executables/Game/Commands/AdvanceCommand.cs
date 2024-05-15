782008eaa78c9ee507fdf10ab841be6ab1636d0e
Nuno Levezinho <nuno.levezinho@live.com.pt>
2024-05-15 11:28:09 +0100

    Introduce /advance command

---
+++ src/Executables/Game/Commands/AdvanceCommand.cs
@@ -0,0 +1,50 @@
+﻿using CommandLine;
+using QuantumCore.API.Game;
+using QuantumCore.API.Game.Types;
+using QuantumCore.API.Game.World;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("advance", "Advances the level of the current player or of another player")]
+[Command("a", "Advances the level of the current player or of another player")]
+public class AdvanceCommand : ICommandHandler<AdvanceCommandOptions>
+{
+    private readonly IWorld _world;
+
+    public AdvanceCommand(IWorld world)
+    {
+        _world = world;
+    }
+
+    public async Task ExecuteAsync(CommandContext<AdvanceCommandOptions> context)
+    {
+        var target = string.Equals(context.Arguments.Target, "$self", StringComparison.InvariantCultureIgnoreCase)
+            ? context.Player
+            : _world.GetPlayer(context.Arguments.Target);
+
+        if (target is null)
+        {
+            context.Player.SendChatInfo("Target not found");
+        }
+        else
+        {
+            if (context.Arguments.Level <= 0)
+            {
+                context.Player.SendChatInfo("Level must be greater than 0");
+                return;
+            }
+            
+            
+            // todo: cap level
+            target.SetPoint(EPoints.Level, (uint) (target.GetPoint(EPoints.Level) + context.Arguments.Level));
+            target.SendPoints();
+            target.SendChatInfo($"You have advanced to level {target.GetPoint(EPoints.Level)}");
+        }
+    }
+}
+
+public class AdvanceCommandOptions
+{
+    [Value(0)] public string Target { get; set; } = "$self";
+    [Value(1, Required = true)] public int Level { get; set; }
+}
