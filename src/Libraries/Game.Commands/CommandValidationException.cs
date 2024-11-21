3b7006121f4ca96bfbcffe6a6d78d91b3f2a7926
Meikel Philipp <MeikelLP@users.noreply.github.com>
2024-11-21 21:02:52 +0100

    fix(game): command validation (#221)
    * feat(docs): config docs Game:Commands:StrictMode
    
    * feat(game): add validation for commands
    
    * chore(docs): change how commands are displayed in the docs
    
    * fix(tests): advance command no longer requires a level amount

---
+++ src/Libraries/Game.Commands/CommandValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+
+namespace QuantumCore.Game.Commands;
+
+public class CommandValidationException : Exception
+{
+    public string Command { get; }
+    public ImmutableArray<string> Errors { get; set; } = [];
+
+    public CommandValidationException(string command) : base("Command arguments validation failed")
+    {
+        Command = command;
+    }
+}
