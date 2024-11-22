913fb11c3c567b4902929bee2a49933941ff7b2c
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-11-22 14:20:42 +0300

    feat(game): bump default attack speed to 100
    Signed-off-by: Utku Korkmaz <uutkukorkmaz@gmail.com>

--- src/Libraries/Game.Server/PlayerConstants.cs
+++ src/Libraries/Game.Server/PlayerConstants.cs
@@ -5,6 +5,6 @@ public static class PlayerConstants
     public const int MAX_PLAYERS_PER_ACCOUNT = 4;
     public const int PLAYER_NAME_MAX_LENGTH = 25;
     
-    public const byte DEFAULT_ATTACK_SPEED = 95;
+    public const byte DEFAULT_ATTACK_SPEED = 100;
     public const byte DEFAULT_MOVEMENT_SPEED = 150;
 }
