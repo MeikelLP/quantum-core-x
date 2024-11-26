a907113940b49d9978137eeb670c092d25e48d90
Meikel Philipp <MeikelLP@users.noreply.github.com>
2024-11-26 17:48:15 +0100

    feat(game): implement /set_maxhp command feat(game): implement /set_maxsp command

---
+++ src/Libraries/Game.Server/Commands/SetMaxSpCommand.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using QuantumCore.API.Game;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("set_maxsp", "Set max sp temporarily")]
+public class SetMaxSpCommand : ICommandHandler<SetMaxSpCommandOptions>
+{
+    public Task ExecuteAsync(CommandContext<SetMaxSpCommandOptions> context)
+    {
+        context.Player.Player.MaxSp = context.Arguments.Value;
+        context.Player.Mana = Math.Min(context.Player.Mana, context.Arguments.Value);
+        context.Player.SendPoints();
+
+        return Task.CompletedTask;
+    }
+}
+
+public class SetMaxSpCommandOptions
+{
+    [Value(0, Required = true)] public uint Value { get; set; }
+}
