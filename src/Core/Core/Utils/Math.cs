52faf939a4dc859f75a5aee88eaa531f2785988b
Meikel Philipp <MeikelLP@users.noreply.github.com>
2024-12-20 13:45:43 +0100

    feat(game): stones and aggro (#248)
    * feat(game): Implement /pull command
    
    * feat(game): Implement /who command
    
    * feat(game): Implement /forgetme command
    
    * fix(game): Renamed /pull command to /aggregate
    
    * feat(game): Implement /pull command
    
    * fix(game): Renamed /guild_create command to /makeguild
    fix(game): Renamed /guild_delete command to /deleteguild
    
    * feat(game): Implement /weak and /weaken command
    
    * feat(game): Implement /ipurge command
    
    * feat(game): Implement /reset and /r command
    
    * fix(game): disconnect would not remove logged in status
    
    * fix(single): InMemoryRedis would calculate expiry wrong
    
    * feat(game): Implement /dc command
    
    * feat(game): Implement stones spawning mobs
    
    * fix: build
    
    * fix(game): null pointer when group.txt is missing
    
    * fix: startup

--- src/Core/Core/Utils/Math.cs
+++ src/Core/Core/Utils/Math.cs
@@ -1,35 +1,51 @@
-﻿namespace QuantumCore.Core.Utils
+﻿namespace QuantumCore.Core.Utils;
+
+public static class MathUtils
 {
-    public static class MathUtils
+    public static double Distance(int x1, int y1, int x2, int y2)
     {
-        public static double Distance(int x1, int y1, int x2, int y2)
-        {
-            var a = x1 - x2;
-            var b = y1 - y2;
+        var a = x1 - x2;
+        var b = y1 - y2;
 
-            return Math.Sqrt(a * a + b * b);
-        }
+        return Math.Sqrt(a * a + b * b);
+    }
 
-        public static double Rotation(double x, double y)
-        {
-            var vectorLength = Math.Sqrt(x * x + y * y);
+    /// <summary>
+    /// Returns the rotation in degrees
+    /// </summary>
+    public static double Rotation(double x, double y)
+    {
+        var vectorLength = Math.Sqrt(x * x + y * y);
 
-            var normalizedX = x / vectorLength;
-            var normalizedY = y / vectorLength;
-            var upVectorX = 0;
-            var upVectorY = 1;
+        var normalizedX = x / vectorLength;
+        var normalizedY = y / vectorLength;
+        var upVectorX = 0;
+        var upVectorY = 1;
 
-            var rotationRadians = -(Math.Atan2(normalizedY, normalizedX) - Math.Atan2(upVectorY, upVectorX));
+        var rotationRadians = -(Math.Atan2(normalizedY, normalizedX) - Math.Atan2(upVectorY, upVectorX));
 
-            var rotationDegress = rotationRadians * (180 / Math.PI);
-            if (rotationDegress < 0) rotationDegress += 360;
-            return rotationDegress;
-        }
-        
-        public static int MinMax(int min, int value, int max)
-        {
-            var temp = (min > value ? min : value);
-            return (max < temp) ? max : temp;
-        }
+        var rotationDegress = rotationRadians * (180 / Math.PI);
+        if (rotationDegress < 0) rotationDegress += 360;
+        return rotationDegress;
+    }
+
+    public static int MinMax(int min, int value, int max)
+    {
+        var temp = (min > value ? min : value);
+        return (max < temp) ? max : temp;
+    }
+
+    public static (double X, double Y) GetDeltaByDegree(double degree)
+    {
+        var radian = DegreeToRadian(degree);
+
+        var x = Math.Sin(radian);
+        var y = Math.Cos(radian);
+        return (x, y);
+    }
+
+    public static double DegreeToRadian(double degree)
+    {
+        return degree * Math.PI / 180.0;
     }
 }
