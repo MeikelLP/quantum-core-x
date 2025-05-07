3d8d1e11f1239e3ac5ff49badefdbbdf36c41047
Meikel Philipp <MeikelLP@users.noreply.github.com>
2025-05-07 19:54:12 +0200

    feat(game): implement /attract_ranger

---
+++ src/Libraries/Game.Server/Commands/AttractRangerCommand.cs
@@ -0,0 +1,19 @@
+﻿using QuantumCore.API.Game;
+using QuantumCore.API.Game.Types;
+using QuantumCore.Game.World.Entities;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("attract_ranger", "Triggers all ranged monsters around you to be aggro and target you")]
+public class AttractRangerCommand : ICommandHandler
+{
+    public Task ExecuteAsync(CommandContext context)
+    {
+        context.Player.ForEachNearbyEntity(e =>
+        {
+            if (e is not MonsterEntity monster || monster.GetBattleType() != EBattleType.Range) return;
+            e.Target = context.Player;
+        });
+        return Task.CompletedTask;
+    }
+}
