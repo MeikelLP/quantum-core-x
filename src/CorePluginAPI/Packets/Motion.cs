299b1b395e23568c2ccfe8d0ec02eae82ebe2cfc
Meikel Philipp <MeikelLP@users.noreply.github.com>
2026-08-06 21:55:47 +0200

    feat: add /motion command

---
+++ src/CorePluginAPI/Packets/Motion.cs
@@ -0,0 +1,12 @@
+﻿using QuantumCore.Networking;
+
+namespace QuantumCore.API.Packets;
+
+[Packet(0x24, EDirection.OUTGOING)]
+[PacketGenerator]
+public partial class Motion
+{
+    [Field(0)] public uint Vid { get; set; }
+    [Field(1)] public uint VictimVid { get; set; }
+    [Field(2)] public ushort MotionId { get; set; }
+}
---
+++ src/Libraries/Game.Server/Commands/MotionCommand.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using QuantumCore.API.Game;
+using QuantumCore.API.Packets;
+using QuantumCore.Game.Extensions;
+
+namespace QuantumCore.Game.Commands;
+
+[Command("motion", "Play a motion animation on your character")]
+public class MotionCommand : ICommandHandler<MotionOptions>
+{
+    public Task ExecuteAsync(CommandContext<MotionOptions> ctx)
+    {
+        var player = ctx.Player;
+        var packet = new Motion
+        {
+            Vid = player.Vid,
+            VictimVid = 0,
+            MotionId = ctx.Arguments.MotionId
+        };
+
+        player.Connection.Send(packet);
+        player.SendChatCommand($"dance1 {player.Vid} 0");
+        foreach (var entity in player.NearbyPlayers)
+        {
+            entity.Connection.Send(packet);
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
+public class MotionOptions
+{
+    [Value(0, Required = true)] public ushort MotionId { get; set; }
+}
