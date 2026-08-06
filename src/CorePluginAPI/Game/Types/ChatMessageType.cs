3bed23471ecf7786b9b033677e61bbbc12aaa6e5
Meikel Philipp <MeikelLP@users.noreply.github.com>
2026-08-06 18:32:29 +0200

    chore: add missing chat message types

--- src/CorePluginAPI/Game/Types/ChatMessageType.cs
+++ src/CorePluginAPI/Game/Types/ChatMessageType.cs
@@ -2,12 +2,13 @@
 
 public enum ChatMessageType : byte
 {
-    NORMAL,
-    INFO,
-
-    // What is type 2?
+    NORMAL = 0,
+    INFO = 1,
+    NOTICE = 2,
     GROUP = 3,
-    GUILD,
-    COMMAND,
-    SHOUT
+    GUILD = 4,
+    COMMAND = 5,
+    SHOUT = 6,
+    WHISPER = 7, // only used on the client
+    BIG_NOTICE = 8
 }
